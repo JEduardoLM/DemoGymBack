49eb0e69b1dbfecc881642d30d8baa8ddaf03e7e
JEduardoLM <li.eduardo.lm@gmail.com>
2016-02-02 23:12:09 -0600

    Elimino los archivos de front, para dejar sólo Back en este repositorio

--- DemoGym/front/modulos/configuracionModulo/js/configuracion.js
+++
@@ -1,8 +0,0 @@
-/*jslint white:true*/
-/*global angular*/
-myApplication.controller('menuCommand', ['$scope', '$window', function($scope, $window){
-    "use strict";
-    $scope.changeMenuHandler = function(){
-        $window.location = "";
-    };
-}]);
